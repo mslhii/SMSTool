50238e2ed30a1d8227dac9257f1fdfa33f5c7d74
mslhii <mslhii@yahoo.com>
2016-04-24 13:27:32 -0700

    Barebones commit for the asynctask

--- app/src/main/java/com/kritikalerror/smstool/MainActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kritikalerror.smstool;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class SMSTask extends AsyncTask<Void, Void, Void> {
+
+        String results;
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            results = "Success";
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            Toast.makeText(MainActivity.this, results, Toast.LENGTH_SHORT).show();
+            super.onPostExecute(result);
+        }
     }
 }
