0e2754dfd9791b613cecfe7f8fffe651e5a565a8
mslhii <mslhii@yahoo.com>
2016-04-25 00:27:42 -0700

    Added button to start the asynctask

--- app/src/main/java/com/kritikalerror/smstool/MainActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/MainActivity.java
@@ -1,16 +1,30 @@
 package com.kritikalerror.smstool;
 
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLoad = (Button)findViewById(R.id.start);
+        buttonLoad.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                SMSTask task = new SMSTask();
+                task.execute();
+            }});
     }
 
     public class SMSTask extends AsyncTask<Void, Void, Void> {
