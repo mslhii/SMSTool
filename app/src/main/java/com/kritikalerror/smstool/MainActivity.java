3b9018a5c2312c2a42265b3a9217ad657d5c8612
mslhii <mslhii@yahoo.com>
2016-07-18 02:08:21 -0700

    Removing unused import

--- app/src/main/java/com/kritikalerror/smstool/MainActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/MainActivity.java
@@ -2,7 +2,6 @@ package com.kritikalerror.smstool;
 
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
