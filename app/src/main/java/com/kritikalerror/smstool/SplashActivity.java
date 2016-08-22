52cd843126470353d1bb981f8bc9fcdffa2abe03
mslhii <mslhii@yahoo.com>
2016-08-22 01:35:46 -0700

    Activity update

--- app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
@@ -1,7 +1,13 @@
 package com.kritikalerror.smstool;
 
-/**
- * Created by mslhi on 8/22/2016.
- */
-public class SplashActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
