6ac5669ec76bd8084228a734551bd35a841dbd90
mslhii <mslhii@yahoo.com>
2016-10-09 10:42:28 -0700

    Removing commented out dialog

--- app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
@@ -28,27 +28,6 @@ public class SplashActivity extends AppCompatActivity {
 
         // Handle permissions issue here
         initializeWrapper();
-
-        // Turn into background?
-        //AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SplashActivity.this);
-        //alertDialogBuilder.setTitle("WARNING!!!");
-        //alertDialogBuilder
-        //        .setMessage("This app is in ALPHA, so it may be unstable. Also I don't take any responsibility if you choose to use this app illegally. If you don't want to use this app press back, otherwise proceed with caution!")
-        //        .setCancelable(false)
-        //        .setPositiveButton("Agree",new DialogInterface.OnClickListener() {
-        //            public void onClick(DialogInterface dialog, int id) {
-        //                dialog.cancel();
-
-        //                Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
-        //                startActivity(myIntent);
-        //            }
-        //        })
-        //        .setNegativeButton("Back",new DialogInterface.OnClickListener() {
-        //            public void onClick(DialogInterface dialog, int id) {
-        //                dialog.cancel();
-        //                finish();
-        //            }
-        //        });
     }
 
     public void proceedButton() {
