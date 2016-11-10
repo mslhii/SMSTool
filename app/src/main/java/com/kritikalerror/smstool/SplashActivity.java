7c1f528576c3017294d6bf173829eb2a2d3e6189
mslhii <mslhii@yahoo.com>
2016-11-10 01:23:18 -0800

    Take out unneeded code

--- app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
@@ -38,7 +38,6 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
                 Intent proceedIntent = new Intent(SplashActivity.this, MainActivity.class);
-                ;
                 startActivity(proceedIntent);
             }
         });
