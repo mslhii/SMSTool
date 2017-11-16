f7fa9274a67840b7146baa3a78bd264dba8a2955
mslhii <mslhii@yahoo.com>
2017-11-16 01:24:13 -0800

    Comment

--- app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/SplashActivity.java
@@ -100,7 +100,7 @@ public class SplashActivity extends AppCompatActivity {
 
     /**
      * Ask user for more permissions
-     * Permissions a
+     * Permissions ar
      * @param permissionsList
      * @param permission
      * @return
