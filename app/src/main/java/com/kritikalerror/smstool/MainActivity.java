443127b0742a8c770a93aed79723df497a7579fc
mslhii <mslhii@yahoo.com>
2019-04-21 00:11:47 -0700

    Comment

--- app/src/main/java/com/kritikalerror/smstool/MainActivity.java
+++ app/src/main/java/com/kritikalerror/smstool/MainActivity.java
@@ -127,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    Core met
+    Core meth
      */
     public class SMSTask extends AsyncTask<Void, Integer, Void> {
         ProgressDialog mProgressDialog;
